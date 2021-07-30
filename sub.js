d631a7c1463e0bea72ed538943ea4a2573d56071
serega.starshov <serega.starshov@mail.ru>
2021-07-30 15:44:59 +0300

    уменьшение строк кода

--- sub.js
+++ sub.js
@@ -1,21 +1,19 @@
 let num = 266219;
-let str = String(num);
-let arr = str.split('');
+let str = String(num).split('');
 
-// let result = arr.reduce((mul, item) => mul * item, 1);
+// let result = str.reduce((mul, item) => mul * item, 1);
 // console.log(result);
 // let numberPow1 = result1 ** 3;
 
 
 let result2 = 1;
 
-for (let i = 0; i < arr.length; i++) {
-    result2 *= arr[i];
+for (let i = 0; i < str.length; i++) {
+    result2 *= str[i];
 }
 console.log(result2);
 
 let numberPow2 = result2 **3;
 console.log(numberPow2);
-let strNumberPow2 = String(numberPow2);
-let twoNumbers = +strNumberPow2.slice(0, 2);
-console.log(twoNumbers);
+let strNumberPow2 = String(numberPow2).slice(0, 2);
+console.log(+strNumberPow2);
