48268fd426d410ceeecae66cade2616b5ed7f5dd
serega.starshov <serega.starshov@mail.ru>
2021-08-03 17:35:31 +0300

    исправлено

--- sub.js
+++ sub.js
@@ -1,29 +1,39 @@
-let str = 'На улице испортилась погода и идет дождь.';
+let str = ' На улице испортилась погода и идет дождь. ';
 let string;
+
+// function transformString(item) {
+//     if (item === '' || Number(item) || item === null) {
+//         alert('Вы ввели неверное значение!');
+//     }
+
+//     let arr = item.split('');
+
+//     for (let i = 0; i < arr.length; i++) {
+
+//         if(arr[i] === " ") {
+//             arr.splice(i, 1);
+//         }
+//     }
+
+//     if (arr.length < 30) {
+//         return arr;
+//     }
+//     string = arr.slice(0, 31).join('');
+//     string += '...';
+//     return string;
+    
+// }
+
+// transformString(str);
+// console.log(string);
+// Как можно сделать решение короче?
 
 function transformString(item) {
     if (item === '' || Number(item) || item === null) {
         alert('Вы ввели неверное значение!');
     }
-
-    let arr = item.split('');
-
-    for (let i = 0; i < arr.length; i++) {
-
-        if(arr[i] === " ") {
-            arr.splice(i, 1);
-        }
-    }
-
-    if (arr.length < 30) {
-        return arr;
-    }
-    string = arr.slice(0, 31).join('');
+    string = item.trim().slice(0, 31);
     string += '...';
-    return string;
-    
 }
-
-transformString(str);
-console.log(string);
-// Как можно сделать решение короче?
+transformString(str)
+console.log(string)
