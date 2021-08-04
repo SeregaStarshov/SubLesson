3eec0782719fb5a35cc33eb5290ff0c18743fd0f
serega.starshov <serega.starshov@mail.ru>
2021-08-05 00:06:22 +0300

    add

--- sub.js
+++ sub.js
@@ -1,3 +1,17 @@
+let number = 100;
+
+check:
+for (let i = 2; i < number; i++) {
+
+    for (let j = 2; j < i; j++) {
+        if(i % j === 0) continue check;
+    }
+    console.log(`${i} Делители этого числа: 1 и ${i}`);
+}
+
+
+
+
 let arr = [];
 
 arr[0] = '439';
@@ -8,6 +22,5 @@ console.log(arr);
 for (let i = 0; i < arr.length; i++) {
     if(arr[i].startsWith(2) || arr[i].startsWith(4)) {
         console.log(arr[i]);
-    }
-    
+    } 
 }
