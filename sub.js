6fc25a27435e9adb65b056d8e7f02fc4613119ad
serega.starshov <serega.starshov@mail.ru>
2021-08-05 00:00:52 +0300

    add

--- sub.js
+++ sub.js
@@ -1,9 +1,13 @@
-let number = 100;
-next:
-for (let i = 2; i < number; i++) {
+let arr = [];
 
-    for (let j = 2; j < i; j++) {
-        if(i % j === 0) continue next;
+arr[0] = '439';
+arr.push('322', '846', '22', '743', '42489', '3854');
+console.log(arr);
+
+
+for (let i = 0; i < arr.length; i++) {
+    if(arr[i].startsWith(2) || arr[i].startsWith(4)) {
+        console.log(arr[i]);
     }
-    console.log(`${i} делитель данного числа 1 и ${i}`);
+    
 }
